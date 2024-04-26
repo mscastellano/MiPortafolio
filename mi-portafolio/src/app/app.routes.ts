8052a5b4f7bd8fdbb0e1e96024d96cb7379f7824
mscastellano <mscastellano@espe.edu.ec>
2024-04-26 15:52:56 -0500

    Actualizacion componentes

--- mi-portafolio/src/app/app.routes.ts
+++ mi-portafolio/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { PortafolioComponent } from './portafolio/portafolio.component';
 import { InformacionComponent } from './portafolio/informacion/informacion/informacion.component';
 
 export const routes: Routes = [
-    {path:"mifoto", component:PortafolioComponent},
-    {path:"information", component:InformacionComponent}
+    { path: '', redirectTo: 'mifoto', pathMatch: 'full' }, // Ruta raíz que redirige a "mifoto"
+    { path: 'mifoto', component: PortafolioComponent },
+    { path: 'information', component: InformacionComponent }
+    
 ];
